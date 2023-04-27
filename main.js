613be08317318b62249c85f75cc2a78e22fabff7
julieta giorgi <juligiorgi2536@gmail.com>
2023-04-27 19:58:14 -0300

    primer cambio

---
+++ main.js
@@ -0,0 +1,26 @@
+let nombre= prompt("Ingrese su nombre");
+if (nombre ===""){
+alert("Nombre incorrecto");
+}else{
+    let mensaje = "Hola como estas?";
+    alert(mensaje);
+}
+
+for(let i=0; i <= 6; i++){
+  let numero = parseInt(prompt("Ingrese numero de productos que hizo"));
+    let resultado= numero*1;
+  alert("Muchas gracias se gano un 30%");
+}
+alert("No hay mas stock")
+
+let resultado=50
+function sumar (numero1, numero2){
+    let resultado= numero1+numero2;
+    console.log("total" + resultado);
+}
+sumar(30,20);
+
+
+
+
+ 
